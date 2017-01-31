2ed0b9dd5728fcf07a2aff36c974a069de71693a
John D <devnull@localhost>
2017-01-31 03:35:29 -0500

    isdicom: move function to own .cpp file as octave <=4.2.0 doesnt PKG_ADD .cpp file directives
    * src/Makefile.in: add isdicom.oct
    
    * src/dicomread.cpp:
      (isdicom): removed function
    
    * src/isdicom.cpp: new file based on isdicom from dicomread.cpp
    
    * NEWS: add description is isdicom move

---
+++ src/isdicom.cpp
@@ -0,0 +1,81 @@
+/*
+ * The GNU Octave dicom package is Copyright Andy Buckle 2010
+ * Contact: blondandy using the sf.net system, 
+ * <https://sourceforge.net/sendmessage.php?touser=1760416>
+ * 
+ * The GNU Octave dicom package is free software: you can redistribute 
+ * it and/or modify it under the terms of the GNU General Public 
+ * License as published by the Free Software Foundation, either 
+ * version 3 of the License, or (at your option) any later version.
+ * 
+ * The GNU Octave dicom package is distributed in the hope that it 
+ * will be useful, but WITHOUT ANY WARRANTY; without even the 
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+ * PURPOSE.  See the GNU General Public License for more details.
+ * 
+ * Please see the file, "COPYING" for further details of GNU General 
+ * Public License version 3.
+ * 
+ */
+
+#include "octave/oct.h"
+#include <octave/ov-struct.h>
+#include <octave/file-stat.h>
+
+#include "gdcmImageReader.h"
+using namespace gdcm;
+
+static bool
+isdicom (const std::string& filename)
+{
+  gdcm::Reader reader;
+  reader.SetFileName (filename.c_str ());
+  // gdcm::Reader.Read() will return false if the file does not exists but
+  // also prints to stderr so we check it first.
+  return file_stat (filename).exists () && reader.Read ();
+}
+
+DEFUN_DLD (isdicom, args, ,
+"-*- texinfo -*- \n\
+@deftypefn {Loadable Function} {} isdicom (@var{filename}) \n\
+Return true if @var{filename} is a valid DICOM file.\n\
+\n\
+@seealso{dicomdict, dicominfo, dicomread, dicomwrite} \n\
+@end deftypefn")
+{
+  const octave_idx_type nargin = args.length ();
+  if (nargin != 1)
+    {
+      print_usage ();
+      return octave_value_list ();
+    }
+
+  // Do NOT have this function accept a cell array of strings.
+  //
+  // This function should return true or false if its input is valid input
+  // for a function that reads a DICOM file.  While it is tempting, and very
+  // easy to implement, to have this function accept a cell array of strings
+  // and return a boolNDArray, a cell array is not valid and will cause any
+  // function that uses it to first check if it's a cell array.  It will also
+  // prevent it from being used by imformats and any other image IO function
+  // in Octave core
+
+  const std::string filename = args(0).string_value ();
+  if (error_state)
+    {
+      error ("isdicom: FILENAME must be a string");
+      return octave_value_list ();
+    }
+
+  return octave_value (isdicom (filename));
+}
+
+/*
+%!test
+%! fpath = tmpnam ();
+%! urlwrite ("http://imagej.nih.gov/ij/images/CT%20Scan.dcm", fpath);
+%! assert (isdicom (fpath), true);
+
+%!test
+%! assert (isdicom (which ("gray")), false);
+*/
